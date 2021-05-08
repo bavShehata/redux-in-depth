ff5547353e5100f63c544617bae7570c269d41d9
bavshehata <bavshehata@gmail.com>
2021-05-08 23:50:19 +0200

    reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -4,3 +4,15 @@ const buyCake = () => ({
   type: BUY_CAKE,
   info: "First redux action",
 });
+
+const initialState = {
+  numOfCakes: 10,
+};
+const reducer = (state = initialState, action = buyCake) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
+    default:
+      return state;
+  }
+};
