b5ed618cac322807150cec0816693eaa9987761a
bavshehata <bavshehata@gmail.com>
2021-05-12 09:22:54 +0200

    multiple reducers

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -4,12 +4,14 @@ import store from "./redux/store";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
+import IcecreamContainer from "./components/IcecreamContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <IcecreamContainer />
         <HooksCakeContainer />
       </div>
     </Provider>
