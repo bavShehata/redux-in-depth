31179806451fc52ba534c95ffe486534386efd0e
bavshehata <bavshehata@gmail.com>
2021-05-12 09:51:12 +0200

    action payload

--- react-redux/src/redux/cake/cakeReducer.js
+++ react-redux/src/redux/cake/cakeReducer.js
@@ -3,7 +3,7 @@ const initialState = { numOfCakes: 10 };
 const cakeReducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
-      return { ...state, numOfCakes: state.numOfCakes - 1 };
+      return { ...state, numOfCakes: state.numOfCakes - action.payload };
     default:
       return state;
   }
