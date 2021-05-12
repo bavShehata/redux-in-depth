5151db91540da7a5087a09149d03f773268dfde9
bavshehata <bavshehata@gmail.com>
2021-05-12 09:23:20 +0200

    multiple reducers

---
+++ react-redux/src/components/IcecreamContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import { buyIcecream } from "../redux";
+const IcecreamContainer = (props) => {
+  return (
+    <div>
+      <h2>Number of icecreams - {props.numOfIcecreams}</h2>
+      <button onClick={props.buyIcecream}>Buy Icecream</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numOfIcecreams: state.icecream.numOfIcecreams,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyIcecream: () => dispatch(buyIcecream()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(IcecreamContainer);
---
+++ react-redux/src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+import cakeReducer from "./cake/cakeReducer";
+import icecreamReducer from "./icecream/icecreamReducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  icecream: icecreamReducer,
+});
+export default rootReducer;
